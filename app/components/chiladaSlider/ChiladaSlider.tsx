1846f82c93301397b65cef95cbaab5438f839582
VickNat <natanimashenafi20@gmail.com>
2024-07-29 00:15:34 +0300

    hi

--- app/components/chiladaSlider/ChiladaSlider.tsx
+++ app/components/chiladaSlider/ChiladaSlider.tsx
@@ -76,10 +76,10 @@ const ChiladaSlider: React.FC = () => {
       centeredSlides: true,
       loop: true,
 
-      // autoplay: {
-      //   delay: 2000,
-      //   disableOnInteraction: false,
-      // },
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
 
       pagination: {
         el: '.swiper-pagination',
